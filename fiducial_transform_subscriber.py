bc914fcd8cec7e985dbe843f4c75441c4eb99262
Sasha <13530248+Alex-web100@users.noreply.github.com>
2022-06-23 06:54:19 -0700

    Add files via upload

--- fiducial_transform_subscriber.py
+++ fiducial_transform_subscriber.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-import tf_conversions
-import tf2_ros
+import tf
 from fiducial_msgs.msg import FiducialTransformArray
 
 def handle_tag_pose(data):
     #Get firs FiducialTransform in the array
-    print("TEST")
-    msg = (data.transforms)[0]
+    """ msg = (data.transforms)[0]
     rospy.loginfo(msg)
+ """
+    try:
+        listener.waitForTransform('fid50','sel_point',rospy.Time(0.0),rospy.Duration(1.0))
+        (trans,rot)=listener.lookupTransform('fid50','sel_point',rospy.Time(0.0))
+        rospy.loginfo(trans)
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        rospy.loginfo("tf exception")
 
 if __name__=='__main__':
     rospy.init_node("fiducial_subscriber")
+
+    listener=tf.TransformListener()
+
     rospy.Subscriber('fiducial_transforms', FiducialTransformArray, handle_tag_pose)
     rospy.spin()
